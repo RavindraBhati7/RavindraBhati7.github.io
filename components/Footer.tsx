7b9bd90cca674ca646ecb3901c30b0b908211b74
Ravindra Bhati <69446199+RavindraBhati7@users.noreply.github.com>
2026-06-30 16:53:32 +0530

    Revert "Update the portfolio "

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-8">
-      <div className="container mx-auto px-6 text-center text-slate-400">© 2026 Ravindra Bhati. All rights reserved.</div>
-    </footer>
-  );
-}
